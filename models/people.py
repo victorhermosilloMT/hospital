9fe8983a9d23cf5cc85916cc03e9faf3093f5903
victorhermosilloMT <becariosvhermosillo@gmail.com>
2020-02-17 09:44:32 -0800

    First commit

---
+++ models/people.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Employee(models.Model):
+    _name = 'hospital.employee'
+    _description = "Hospital Employee"
+
+    id = fields.Integer(string="Id")
+    name = fields.Char(string="Name:", required=True)
+    lastName = fields.Char(string="Last name:")
+    phone = fields.Char(string="Phone:")
+    age = fields.Integer(string="Age:")
+    file_ids = fields.One2many('hospital.file', 'employee_id', string="Cases attending:")
+
+class Patient(models.Model):
+    _name = 'hospital.patient'
+    _description = "Hospital Patient"
+
+    id = fields.Integer(string="Id")
+    name = fields.Char(string="Name:", required=True)
+    lastName = fields.Char(string="Last name:")
+    phone = fields.Char(string="Phone:")
+    age = fields.Integer(string="Age:")
+    problem = fields.Text(string="Sintoms:", required=True)
+    status = fields.Boolean(default=True)
+
+    files_id = fields.One2many('hospital.file', 'patients_id', ondelete='cascade', string="Cases:")
+
+class File(models.Model):
+    _name = 'hospital.file'
+    _description = "Hospital Files"
+
+    id = fields.Integer(string="Id")
+    name = fields.Char(string="File name:", required=True)
+    info = fields.Text(string="File info:")
+    employee_id = fields.Many2one('hospital.employee', ondelete='set null', string="Employee in charge:", required=True)
+    patients_id = fields.Many2one('hospital.patient', ondelete='set null', string="Patient attending:", required=True)
