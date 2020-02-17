f9df30377c2bb45580a4d45b4e4e99b85546540c
victorhermosilloMT <becariosvhermosillo@gmail.com>
2020-02-17 11:51:18 -0800

    Xml separation

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/hospital.xml',
+        'views/employee.xml',
+        'views/patient.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
