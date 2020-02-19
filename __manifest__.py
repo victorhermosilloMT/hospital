dbdcfb5abeaeac9b6c0950d2191f813f9ef63587
victorhermosilloMT <becariosvhermosillo@gmail.com>
2020-02-19 08:28:57 -0800

    Separation of views and add icon module

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hospital",
+    'name': "Hospital",
 
-    'summary': """MAnage hospital information""",
+    'summary': """Manage hospital information""",
 
     'description': """Manage the files and patients of an hospital""",
 
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '12.0.0.1',
+    'version': '12.0.0.1.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -23,6 +23,7 @@
         'security/ir.model.access.csv',
         'views/employee.xml',
         'views/patient.xml',
+        'views/file.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
